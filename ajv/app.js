7e67d3eac3a5b0821f4f658fac3342560d1515a8
Eliezer Marte <e.marte@fld-ml-00054010.fios-router.home>
2019-08-12 12:49:27 -0400

    Added Call To BIG-IP

--- ajv/app.js
+++ ajv/app.js
@@ -1,22 +1,68 @@
 var Ajv = require('ajv');
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser')
+
+//JSON Schema
 const f5 = require('./f5.json')
-const payload = require('./test.json')
+
+//Express variables 
+var app = express();
+var port = process.env.PORT || 3000;
+
 var ajv = new Ajv();
 
-var validate = ajv.addFormat('f5bigip', /./)
-.addFormat('f5label', /./)
-.addFormat('f5remark', /./)
-.addFormat('f5ip', /./)
-.addFormat('f5pointer', /./)
-.addFormat('f5base64', /./)
-.addFormat('f5name', /./)
-.addFormat('f5long-id', /./)
+//Formats needed for Ajv to work with Schema
+//TODO add more formats
+var ajv = new Ajv({ 
+  allErrors: true, 
+  unknownFormats: [
+      'f5bigip',
+      'f5label',
+      'f5remark',
+      'f5ip',
+      'f5pointer',
+      'f5base64',
+      'f5name',
+      'f5long-id'
+  ] 
+});
+
+var validate = ajv
 .compile(f5);
 
-test(payload);
+//Parse JSON
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-function test(data) {
-  var valid = validate(data);
-  if (valid) console.log('Valid!');
-  else console.log('Invalid: ' + ajv.errorsText(validate.errors));
-}
+//POST route
+app.post('/eliezersbaby/verify', function (req, res) {
+  test(req.body);
+
+  function test(data) {
+    var valid = validate(data);
+    if (valid) {
+      request({
+        url: "http://10.1.1.245/mgmt/shared/appsvcs/declare",
+        method: "POST",
+        json: true,  
+        auth: {'user': 'admin',
+        'pass': 'admin',},
+        body: payload
+    }, function (error, response, body){
+        //console.log(response);
+    });
+
+    }
+
+    else console.log('Invalid: ' + ajv.errorsText(validate.errors));
+  }
+
+  res.send("Now Deploying to BIG-IP")
+})
+
+
+
+app.listen(port, ()=>{
+  
+});
