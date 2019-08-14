76052d90c730ff61fd64f9be16e40898dd041e99
Eliezer Marte <ezmarte@gmail.com>
2019-08-14 13:00:11 -0400

    added json parser in front-end

--- src/index.js
+++ src/index.js
@@ -3,75 +3,33 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 import axios from 'axios'
 
-
 class MyForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      errormessage: ""
+    this.state = { value: ''
     };
+    this.myChangeHandler = this.myChangeHandler.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+  
   myChangeHandler = event => {
-    if (event.target.value) {
-      this.setState({ errormessage: " Not Valid" });
-    }
+      this.setState({ value: event.target.value});
+      
+      
   };
-    handleSubmit = event =>  {
-      event.preventDefault();
-
-      var payload = {
-        
-          "class": "AS3",
-          "action": "deploy",
-          "persist": true,
-          "declaration": {
-             "class": "ADC",
-             "schemaVersion": "3.0.0",
-             "id": "urn:uuid:33045210-3ab8-4636-9b2a-c98d22ab915d",
-             "label": "Sample 1",
-             "remark": "Simple HTTP application with RR pool",
-             "Sample_01": {
-                "class": "Tenant",
-                "A1": {
-                   "class": "Application",
-                   "template": "http",
-                   "serviceMain": {
-                      "class": "Service_HTTP",
-                      "virtualAddresses": [
-                         "10.0.1.10"
-                      ],
-                      "pool": "web_pool"
-                   },
-                   "web_pool": {
-                      "class": "Pool",
-                      "monitors": [
-                         "http"
-                      ],
-                      "members": [{
-                         "servicePort": 80,
-                         "serverAddresses": [
-                            "192.0.1.10",
-                            "192.0.1.11"
-                         ]
-                      }]
-                   }
-                }
-             }
-          }
-       
-      };
-
-      //payload = JSON.stringify(payload);
-
-      axios.post(`http://localhost:4000/eliezersbaby/verify`, payload)
+  handleSubmit(event) {
+    var payload= JSON.parse(this.state.value);
+    event.preventDefault();
+ 
+    console.log(this.state.value)
+          axios.post(`http://localhost:4000/eliezersbaby/verify`, payload)
         .then(res => {
           //console.log(res);
-          console.log(res.data);
+          //console.log(res.data);
         })
-        console.log(payload)
-    }
+  }
 
-
+     
   render() {
     return (
       <div>
@@ -79,8 +37,7 @@ class MyForm extends React.Component {
           <h1>AS3 Validator</h1>
           <p>
             Your Declaration is:
-            {//this.state.errormessage
-            }
+            {this.state.errormessage}
           </p>
           <textarea className="textboxclass" name="name" value={this.state.value} onChange={this.myChangeHandler} cols='50' rows='10'  />
           <input
